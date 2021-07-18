91df9288e1b18a28c8277bad59077101c96b3178
Quasar0007 <me2191079@iitd.ac.in>
2021-07-18 16:11:46 +0530

    Fully_Responsive

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,8 +6,8 @@ function Footer() {
     return (
         <section id="Footer">
             <div className="blue">
-                <p><a href="#navbar">Home</a><span className="space">|</span><a href="#Intro">Meet Our Team</a></p>
-                <p>Our Vision<span  className="space">|</span>Our Mission<span className="space">|</span>Privacy Policy</p>
+                <p className="txt-foot"><a href="#navbar">Home</a><span className="space">|</span><a href="#Intro">Meet Our Team</a></p>
+                <p className="txt-foot">Our Vision<span  className="space">|</span>Our Mission<span className="space">|</span>Privacy Policy</p>
                 <img src={AI} className="img4" alt="Not Found"/>
                 <img src={AI} className="img5" alt="Not Found"/>
 
